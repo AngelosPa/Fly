fb3345051b31fb8abcfd4f9173a08e2a078f8cd4
angelo.p <mrpalaiologos@gmail.com>
2021-08-20 15:24:33 +0200

    DONE :D

--- src/lib/date.js
+++ src/lib/date.js
@@ -6,32 +6,9 @@ let axios = require("axios").default;
 // const destination = require("./destination");
 // let airport = require("./flights");
 
-const [place, date] = args;
-//to get the dates
-let options = {
-  method: "GET",
-  url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/GR/USD/en-US/BERL-sky/TYOA-sky/anytime`,
-  params: { inboundpartialdate: `anytime` },
-  headers: {
-    "x-rapidapi-key": xrapidapikey,
-    "x-rapidapi-host": xrapidapihost,
-  },
-};
+const [place, destina] = args;
 
-axios
-  .request(options)
-  .then(function (response) {
-    const datums = response.data.Quotes.map((el) => {
-      return el.QuoteDateTime;
-    });
-    // console.log(datums.map((el) => el.replace(/T/, " at ")));
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-//to get the airport name with city name as a parameter
-let optionsforairport = {
+let optionsDeparture = {
   method: "GET",
   url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/`,
   params: { query: `${place}` },
@@ -40,38 +17,62 @@ let optionsforairport = {
     "x-rapidapi-host": xrapidapihost,
   },
 };
-async function getAirportName() {
-  await axios
-    .request(optionsforairport)
-    .then(function (response) {
-      const Places = response.data.Places.map((el) => {
-        return el.CityId;
-      });
-      console.log(Places[0]);
-    })
-    .catch(function (error) {
-      console.error(error);
+let optionsDestination = {
+  method: "GET",
+  url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/`,
+  params: { query: `${destina}` },
+  headers: {
+    "x-rapidapi-key": xrapidapikey,
+    "x-rapidapi-host": xrapidapihost,
+  },
+};
+function getTravelDate() {
+  //here we are getting the departure airport id from users  first argument
+  return axios.request(optionsDeparture).then(function (response) {
+    const PlacesDeparture = response.data.Places.map((el) => {
+      if (el.PlaceName.includes(optionsDeparture.params.query)) {
+        return `${el.CityId}`;
+      } else {
+        return null;
+      }
     });
+    //console.log(PlacesDeparture);
+    //here we are getting the Destination airport id from users second argument
+    axios.request(optionsDestination).then(function (response) {
+      const PlaceDestination = response.data.Places.map((el) => {
+        if (el.PlaceName.includes(optionsDestination.params.query)) {
+          return `${el.CityId}`;
+        } else {
+          return null;
+        }
+      });
+      // console.log(PlaceDestination);
+      //now that we have both departure and destination we can get the travel possible dates by assign the variable with the airport codes to the special data feed for the dates
+      let options = {
+        method: "GET",
+        url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/GR/USD/en-US/${PlacesDeparture[0]}/${PlaceDestination[0]}/anytime`,
+        params: { inboundpartialdate: `anytime` },
+        headers: {
+          "x-rapidapi-key": xrapidapikey,
+          "x-rapidapi-host": xrapidapihost,
+        },
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          const datums = response.data.Quotes.map((el) => {
+            return `There is a flight from ${place} to ${destina} on ${el.QuoteDateTime.replace(
+              /T/,
+              " at "
+            )} for ${el.MinPrice}$`;
+          });
+          console.log(datums);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    });
+  });
 }
-getAirportName();
-
-// let optionsforairport = {
-//   method: "GET",
-//   url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/",
-//   params: { query: `${place}` },
-//   headers: {
-//     "x-rapidapi-key": xrapidapikey,
-//     "x-rapidapi-host": xrapidapihost,
-//   },
-// };
-
-// axios
-//   .request(optionsforairport)
-//   .then(function (response) {
-//     const places = response.data.Places;
-//     console.log(places[0].PlaceId);
-//   })
-
-//   .catch(function (error) {
-//     console.error(error);
-//   });
+getTravelDate();
